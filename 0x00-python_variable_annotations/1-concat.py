0e96f723773bc39fe1ec31fe1cc8e87510592c1f
ericvenom99 <prevenakhtar@mail.com>
2024-10-11 04:39:38 +0200

    uploaded task 1 and 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+ function concatenated strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function  returns a concatenated string.
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+ This function uses math.floor function to get the floor of a float
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the input float.
+    """
+    return math.floor(n)
