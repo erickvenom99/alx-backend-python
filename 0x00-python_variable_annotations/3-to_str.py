9dd210f6621a203d2c79a8349d9d55b262d00162
ericvenom99 <prevenakhtar@mail.com>
2024-10-11 04:40:13 +0200

    updated task m

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+ Function a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function takes a float argument and returns its string representation.
+    """
+    return str(n)
