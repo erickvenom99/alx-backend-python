ebae3cb87f974d76621068ed7579d7d504efe336
ericvenom99 <prevenakhtar@mail.com>
2024-10-15 06:09:27 +0200

    correctes task1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -25,38 +25,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     for task in delay_task:
         delay_list.append(task)
-    merge_sort(delay_list)
-    return delay_list
-
-
-def merge_sort(arr: List[float]) -> None:
-    """
-    Sorts an array in place using the merge sort algorithm.
-    """
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        i = j = k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
+    return sorted(delay_list)
