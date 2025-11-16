a2a65be2dbf1278d90240e5d5261453295082f95
erickvenom <prevenakhtar@mail.com>
2025-11-16 08:44:09 +0000

    Ensure file meets pycode standared

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -57,18 +57,24 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
+    """Test the decorator memoize"""
     def test_memoize(self):
         """Test that memoize caches result and calls method only once."""
         class TestClass:
             def a_method(self):
                 return 42
-            @memoize 
+
+            @memoize
             def a_property(self):
-             return self.a_method()
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
-                obj = TestClass()
-                first_result = obj.a_property
-                second_result = obj.a_property
-                self.assertEqual(first_result, 42)
-                self.assertEqual(second_result, 42)
-                mock_method.assert_called_once()
+                return self.a_method()
+        with patch.object(
+            TestClass,
+            'a_method',
+            return_value=42
+        )as mock_method:
+            obj = TestClass()
+            first_result = obj.a_property
+            second_result = obj.a_property
+            self.assertEqual(first_result, 42)
+            self.assertEqual(second_result, 42)
+            mock_method.assert_called_once()
